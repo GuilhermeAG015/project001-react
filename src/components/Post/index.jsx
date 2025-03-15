bf3887db4c041ae8a49834fecb62102dfa11223f
Guilherme Alexandre <guilhermegrilo2009@hotmail.com>
2025-03-15 16:47:57 -0300

    Adding date-fns lib

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,31 +1,45 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale/pt-BR'
+
 import { Avatar } from '../Avatar'
 import styles from './Post.module.css'
 import { Comment } from './SubComponents/Comment'
 
 export function Post({ author, content, publishedAt }) {
+  const formattedDate = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src='https://github.com/GuilhermeAG015.png' />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Guilherme Alexandre</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title='03 de Março às 8:13h' dateTime='2025-03-03 08:13:30'>Publicado há 1h</time>
+        <time title={formattedDate} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href='/'>jane.design/doctorcare</a></p>
-        <p>
-          <a href='/'>#novoprojeto</a>{' '}
-          <a href='/'>#nlw</a>{' '}
-          <a href='/'>#rocketseat</a>
-        </p>
+        {content.map(item => {
+          if (item.type === 'paragraph') {
+            return <p>{item.content}</p>
+          // biome-ignore lint/style/noUselessElse: <explanation>
+          } else if (item.type === 'link') {
+            return <p><a href='./'>{item.content}</a></p>
+          }
+        })}
       </div>
       
       <form className={styles.commentForm}>
